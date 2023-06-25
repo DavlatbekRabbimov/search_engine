d01fe9bf6e76687fb413fe0cc3b00ace03f2c4a2
DevLope <dvsloppo@gmail.com>
2023-06-25 20:42:30 +0500

    search_engine updated
    Method compute was changed

--- src/main/java/searchengine/services/parser/SiteRecursiveTask.java
+++ src/main/java/searchengine/services/parser/SiteRecursiveTask.java
@@ -3,16 +3,17 @@ package searchengine.services.parser;
 import lombok.AllArgsConstructor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import searchengine.model.*;
-
+import searchengine.model.Status;
 import searchengine.model.entity.Site;
 import searchengine.model.repo.PageRepo;
 import searchengine.model.repo.SiteRepo;
 import searchengine.services.serviceimpl.DbService;
 
 import java.io.IOException;
-import java.util.*;
-import java.util.concurrent.*;
+import java.util.Set;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.RecursiveAction;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
@@ -31,50 +32,46 @@ public class SiteRecursiveTask extends RecursiveAction {
 
     @Override
     protected void compute() {
+        if (isInterrupted || pageRepo.existsByPathAndSite(path, site)) return;
         try {
-            if (isInterrupted && pageRepo.existsByPathAndSite(path, site)) return;
-            db.savePageToDb(getDocument(), site, path);
-            invokeAll(getAbsUrl(getDocument()).stream()
-                    .map(this::getRecursiveByUrl)
-                    .collect(Collectors.toSet()));
-        } catch (CancellationException ignore) {
-        } catch (Exception e) {
-            siteRepo.findSiteByUrl(site.getUrl()).ifPresent(site -> {
-                        site.setStatus(Status.FAILED);
-                        site.setLastError("Тип ошибки: " + e.getMessage());
-                        siteRepo.saveAndFlush(site);
-                    });
+            Document document = getDocument();
+            db.savePageToDb(document, site, path);
+            CopyOnWriteArraySet<SiteRecursiveTask> subTasks = new CopyOnWriteArraySet<>();
+            getAbsUrl(document).parallelStream().map(this::getRecursiveByUrl).forEach(subTasks::add);
+            invokeAll(subTasks);
+        } catch (CancellationException | IOException e) {
+            handleException(e);
         }
     }
+
     private SiteRecursiveTask getRecursiveByUrl(String url) {
         String processedUrl = url.equals(site.getUrl()) ? "/" : url.substring(site.getUrl().length());
         return new SiteRecursiveTask(siteRepo, pageRepo, db, site, processedUrl);
     }
-    private Document getDocument() {
+
+    private Document getDocument() throws IOException {
         try {
             Thread.sleep(WAIT_TIME_MS);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
-        try {
-            return Jsoup.connect(site.getUrl().concat(path))
-                    .userAgent(USER_AGENT)
-                    .referrer(REFERRER)
-                    .ignoreHttpErrors(true)
-                    .ignoreContentType(true)
-                    .followRedirects(false)
-                    .timeout(CONNECTION_TIMEOUT_MS)
-                    .get();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+
+        return Jsoup.connect(site.getUrl().concat(path))
+                .userAgent(USER_AGENT)
+                .referrer(REFERRER)
+                .ignoreHttpErrors(true)
+                .ignoreContentType(true)
+                .followRedirects(false)
+                .timeout(CONNECTION_TIMEOUT_MS)
+                .get();
     }
+
     private Set<String> getAbsUrl(Document document) {
         return document.select("a[href]")
                 .stream()
                 .map(attribute -> attribute.absUrl("href"))
                 .filter(this::isCorrectUrl)
-                .collect(Collectors.toSet());
+                .collect(Collectors.toCollection(CopyOnWriteArraySet::new));
     }
 
     private boolean isCorrectUrl(String url) {
@@ -83,6 +80,13 @@ public class SiteRecursiveTask extends RecursiveAction {
                 !url.contains("#") &&
                 !url.matches("([\\w\\W]+(\\.(?i)(jpg|jpe?g|png|gif|bmp|pdf|doc|docx|DOC|PNG|JPE?G|JPG|php[\\W\\w]|#[\\w\\W]*|\\?[\\w\\W]+)))$");
     }
-}
 
+    private void handleException(Exception e) {
+        siteRepo.findSiteByUrl(site.getUrl()).ifPresent(site -> {
+            site.setStatus(Status.FAILED);
+            site.setLastError("Тип ошибки: " + e.getMessage());
+            siteRepo.saveAndFlush(site);
+        });
+    }
+}
 
---
+++ src/main/java/searchengine/services/serviceimpl/DbServiceImpl.java
@@ -0,0 +1,101 @@
+package searchengine.services.serviceimpl;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.safety.Safelist;
+import org.springframework.stereotype.Service;
+import searchengine.config.Uri;
+import searchengine.model.*;
+import searchengine.model.entity.Index;
+import searchengine.model.entity.Lemma;
+import searchengine.model.entity.Page;
+import searchengine.model.entity.Site;
+import searchengine.model.repo.IndexRepo;
+import searchengine.model.repo.LemmaRepo;
+import searchengine.model.repo.PageRepo;
+import searchengine.model.repo.SiteRepo;
+import searchengine.services.service.DbService;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Service
+public class DbServiceImpl implements DbService {
+    private final SiteRepo siteRepo;
+    private final PageRepo pageRepo;
+    private final LemmaRepo lemmaRepo;
+    private final IndexRepo indexRepo;
+    private final LemmatizerServiceImpl lemmatizer;
+    private static final Integer BAD_REQUEST = 400;
+
+    public void saveSiteToDb(Uri uri, Status status) {
+        String processedUrl = StringUtils.stripEnd(uri.getUrl(), "/");
+        siteRepo.saveAndFlush(new Site(status, new Date(), "", processedUrl, uri.getName()));
+    }
+
+    public void savePageToDb(Document document, Site site, String path) {
+        String processedPath = StringUtils.defaultIfBlank(path, "/");
+        int statusCode = document.connection().response().statusCode();
+
+        siteRepo.findSiteByUrl(site.getUrl()).ifPresent(siteEntity -> {
+            siteEntity.setLastError(null);
+            siteEntity.setStatusTime(new Date());
+            siteRepo.saveAndFlush(siteEntity);
+        });
+
+        Page page = pageRepo.findByPathAndSite(processedPath, site).orElseGet(()
+                -> new Page(site, processedPath, statusCode, document.html()));
+        pageRepo.saveAndFlush(page);
+
+        if (page.getCode() < BAD_REQUEST) {
+            saveLemmaToDb(page);
+            saveIndexToDb(page);
+        }
+    }
+
+    private void saveLemmaToDb(Page page) {
+        Set<Lemma> lemmas = ConcurrentHashMap.newKeySet();
+        synchronized (this) {
+            getNormalizedWordCounts(page)
+                    .entrySet()
+                    .stream()
+                    .filter(entry -> !SiteRecursiveTask.isInterrupted)
+                    .map(entry -> lemmaSaverSetting(page, entry.getKey()))
+                    .forEach(lemmas::add);
+            lemmaRepo.saveAllAndFlush(lemmas);
+        }
+    }
+
+    private void saveIndexToDb(Page page) {
+        Set<Index> indexes = ConcurrentHashMap.newKeySet();
+        synchronized (this) {
+            getNormalizedWordCounts(page).forEach((lemma, rank) -> {
+                if (SiteRecursiveTask.isInterrupted) return;
+                indexes.add(new Index(page, lemmaSaverSetting(page, lemma), rank));
+            });
+            indexRepo.saveAllAndFlush(indexes);
+        }
+    }
+
+    @Override
+    public Map<String, Integer> getNormalizedWordCounts(Page page) {
+        return lemmatizer.getNormalWordCounts(Jsoup.clean(page.getContent(), Safelist.relaxed()).replaceAll("ё", "е"));
+    }
+
+    @Override
+    public Lemma lemmaSaverSetting(Page page, String lemmaName) {
+        return lemmaRepo.findOptByLemma(lemmaName).map(lemma -> {
+            lemma.setFrequency(lemma.getFrequency() + 1);
+            return lemma;
+        }).orElse(new Lemma(page.getSite(), lemmaName, 1));
+    }
+}

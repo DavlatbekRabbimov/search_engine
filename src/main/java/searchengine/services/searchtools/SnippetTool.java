8ee0d46bf3de2d25da6c8a0cf348014ab6486436
DevLope <dvsloppo@gmail.com>
2023-06-27 12:43:18 +0500

    search_engine updated
    changed SiteTool

--- src/main/java/searchengine/services/searchtools/SnippetTool.java
+++ src/main/java/searchengine/services/searchtools/SnippetTool.java
@@ -1,5 +1,4 @@
 package searchengine.services.searchtools;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -13,62 +12,57 @@ import java.util.concurrent.atomic.AtomicReference;
 @RequiredArgsConstructor
 public class SnippetTool {
     private final LemmatizerServiceImpl lemmatizer;
+    private final static int SNIPPET_LENGTH = 260;
+    private final static int LIMIT_IN_DISTANCE_BEFORE_BOLD = 50;
 
     private String getSnippet(String text) {
         int startIndex = text.indexOf("<b>");
         int endIndex = text.indexOf("</b>", startIndex);
 
         if (startIndex >= 0 && endIndex >= 0) {
-            startIndex += "<b>".length();
-            String snippet = text.substring(startIndex, endIndex).replaceAll("\\s+", " ");
-            String nextTextPart = text.substring(getIndexFirstSpace(text, endIndex), text.length() - 1);
-            String snippetWithoutBold = nextTextPart.replaceAll("</?b>", "");
-            return snippetHandler(snippet, snippetWithoutBold, nextTextPart);
+            int wordIndexInFirstBold = startIndex + "<b>".length();
+            String wordInFirstBold = text.substring(wordIndexInFirstBold, endIndex).replaceAll("\\s+", " ");
+            int firstBoldIndexInText = text.indexOf("<b>", text.indexOf(getBoldTexts(text, startIndex, endIndex)));
+            int spaceIndex = text.indexOf(" ", Math.max(0, firstBoldIndexInText - LIMIT_IN_DISTANCE_BEFORE_BOLD));
+            String nextTextPart = text.substring(spaceIndex, text.length() - 1);
+            String snippetWithoutBold = nextTextPart.replaceAll("</?b>", "").trim();
+            return snippetHandler(wordInFirstBold, snippetWithoutBold, nextTextPart);
         } else {
-            if (text.length() > 260) return text.substring(0, 260);
+            if (text.length() > SNIPPET_LENGTH) return text.substring(0, SNIPPET_LENGTH);
             else return text;
         }
     }
-
-    public String snippetHandler(String snippet, String snippetWithoutBold, String nextTextPart) {
-        int boldCount = StringUtils.countOccurrencesOf(nextTextPart, "<b>");
-
-        if (boldCount == 0) return nextTextPart.length() > 260 ? nextTextPart.substring(0, 260) : nextTextPart;
-
-        while (boldCount >= 1) {
-            int indexFirstSpace = nextTextPart.indexOf(" ", 260 + boldCount * 7);
-            if (indexFirstSpace != -1) snippet = nextTextPart.substring(0, indexFirstSpace);
-            else snippet = nextTextPart;
-            if (snippet.replaceAll("</?b>", "").length() < snippetWithoutBold.length()) break;
-            boldCount--;
-        }
-
-        String[] words = snippet.split("\\s+");
-        StringBuilder uniqueSnippet = new StringBuilder();
-        Set<String> uniqueWords = new HashSet<>();
-
-        for (String word : words) {
-            if (uniqueWords.add(word)) uniqueSnippet.append(word).append(" ");
-        }
-        return uniqueSnippet.toString().replaceAll("\\s+", " ").trim();
-    }
-
-    private int getIndexFirstSpace(String text, int endIndex) {
-        int startIndex = text.indexOf("<b>");
-        if (startIndex >= 0 && endIndex >= 0) {
-            int boldTextCount = text.indexOf("<b>", text.indexOf(getBoldTexts(text, startIndex, endIndex)));
-            int findFromThisIndex = Math.max(0, boldTextCount - 50);
-            return text.indexOf(" ", findFromThisIndex);
-        }
-        return 0;
-    }
-
 
     public String getBoldTexts(String text, int start, int end) {
         return Arrays.stream(text.substring(start, end).split("(?<=[.!?·])\\s*(?=(<b>|\"|«|· )?[А-ЯЁ])"))
                 .filter(partText -> StringUtils.countOccurrencesOf(partText, "<b>") > 0)
                 .findFirst()
                 .orElse("");
+    }
+
+    public String snippetHandler(String snippet, String snippetWithoutBold, String nextTextPart) {
+
+        int boldCount = StringUtils.countOccurrencesOf(nextTextPart, "<b>");
+        int boldLength = "<b>".length() + "</b>".length();
+        int snippetLengthWithBold = SNIPPET_LENGTH + boldCount + boldLength;
+
+        if (boldCount == 0) return nextTextPart.length() > SNIPPET_LENGTH
+                ? nextTextPart.substring(0, SNIPPET_LENGTH)
+                : nextTextPart;
+
+        while (boldCount >= 1) {
+            int spaceIndex = nextTextPart.indexOf(" ", snippetLengthWithBold);
+            if (spaceIndex != -1) snippet = nextTextPart.substring(0, spaceIndex);
+            else snippet = nextTextPart;
+            if (snippet.replaceAll("</?b>", "").length() < snippetWithoutBold.length()) break;
+            boldCount--;
+        }
+
+        StringBuilder uniqueSnippet = new StringBuilder();
+        for (String word : snippet.split("\\s+")) {
+            uniqueSnippet.append(word).append(" ");
+        }
+        return uniqueSnippet.toString().replaceAll("\\s+", " ").trim();
     }
 
     private String getRussianWords(String text) {
@@ -78,6 +72,7 @@ public class SnippetTool {
     private String tagQuery(String word, String lemma, Map<String, Set<String>> generatedWords) {
         Set<String> lemmas = generatedWords.get(lemma);
         word = getRussianWords(word);
+
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
@@ -102,7 +97,6 @@ public class SnippetTool {
         }
         return getSnippet(combinedWords.toString()).replaceAll("\\s+", " ").concat(" ...").trim();
     }
-
 
 }
 
